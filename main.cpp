d137d41d5ea39f5e219aad6e5ee030091b07f0f3
DeltaHit <DeltaHit@users.noreply.github.com>
2018-10-19 13:31:49 -0400

    starting structure

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+Auteur:		Émile Loiselle
+Date:		//
+Programme:	main.cpp
+But:		//
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "fra");
+
+	//Your code here...
+
+	return 0;
+}
