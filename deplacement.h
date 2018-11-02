026c17f3e4e613f564cb94efdd6bacbc46a3ab2e
DeltaHit <DeltaHit@users.noreply.github.com>
2018-11-02 15:54:52 -0400

    init de deplacement

--- deplacement.h
+++ deplacement.h
@@ -13,16 +13,63 @@ class deplacement
 private:
 	int _x;
 	int _y;
+	char _orientation;
 
 public:
 	deplacement();
+	deplacement(int, int, char);
 	~deplacement();
+
+	int getX() const;
+	int getY() const;
+	char getOrientation() const;
+
+	void setX(int x);
+	void setY(int y);
+	void setOrientation(char orientation);
 };
 
 deplacement::deplacement()
 {
 }
 
+deplacement::deplacement(int x, int y, char orientation)
+{
+	_x = x;
+	_y = y;
+	_orientation = orientation;
+}
+
 deplacement::~deplacement()
 {
+}
+
+int deplacement::getX() const
+{
+	return _x;
+}
+
+int deplacement::getY() const
+{
+	return _y;
+}
+
+char deplacement::getOrientation() const
+{
+	return _orientation;
+}
+
+void deplacement::setX(int x)
+{
+	_x = x;
+}
+
+void deplacement::setY(int y)
+{
+	_y = y;
+}
+
+void deplacement::setOrientation(char orientation)
+{
+	_orientation = orientation;
 }
