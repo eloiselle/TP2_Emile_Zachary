7dbc9c0a479c8ad39a78b6ab0d6a098a1c5cdfdd
DeltaHit <DeltaHit@users.noreply.github.com>
2018-11-05 10:09:51 -0500

    Modified pile.h

--- pile.h
+++ pile.h
@@ -5,19 +5,26 @@
 	But			: Objet qui permet d'ajouter, enlever
 				  et modifier l'element du dessus d'une pile.
 ==================================================================*/
-#pragma once
-#include <assert.h>
+/*
+Auteur:		Émile Loiselle
+Date:		1 novembre 2018
+Programme:	pile.h
+But:		Manipuler une pile de manière "First In First Out"
+			avec des opérations de base.
+*/
 
-using namespace std;
+#pragma once
+#include <iostream>
+#include <assert.h>
 
 template <class TYPE>
 class pile
 {
-private:
+protected:
 	struct cellule
 	{
-		TYPE element;	// élément ds la cellule
-		cellule *next; 	// pointeur vers prochaine cellule
+		TYPE element;	//élément ds la cellule
+		cellule	*next; 	//pointeur vers prochaine cellule
 
 		cellule(const TYPE& e, cellule * n)
 		{
@@ -25,127 +32,147 @@ private:
 			next = n;
 		}
 	};
-	cellule  *_top;				// 1er élément de la pile
-	int  	  _size;			// taille (nb. élément) de la pile
+
+	cellule	*_top;	//1er élément de la pile
+	int	_size;		//taille (nb. élément) de la pile
+
+	//Copieur protégé
+	pile(const pile<TYPE>& p);
+
 public:
-	pile();						// constructeurr & destructeurr
+
+	//Constructeurs et destructeur
+	pile();
 	~pile();
 
-	void push(const TYPE& e);	//ajoute un nouveau dessus
-	TYPE pop();					//enlève le dessus
+	void push(const TYPE& e); //Ajoute un dessus
+	void pop(); //Enlève un dessus
 
-	const TYPE& top() const;	//retourne le dessus, mais dépile pas
-	TYPE& top();				//retourne le dessus, mais en setteur
-	int size()const;			//retourne le nb d’élément
+	const TYPE& top() const;
+	TYPE& top();
+	int size() const;
 
-	bool empty() const;			//si la pile est vide
-	void clear();				//vide la pile
+	bool isEmpty() const;
+	void clear();
 
-	const pile& operator=(const pile& s);	//affectateur
-	bool operator==(const pile& s)const;	//comparaison de 2 piles
+	//const pile<TYPE>& operator=(const pile& s);
+	//bool operator==(const pile& s)const;
 };
 
-//Constructeur
-template <class TYPE>
+template<class TYPE>
 pile<TYPE>::pile()
 {
-	next = nullptr;
+	_top = nullptr;
 	_size = 0;
 }
 
-//Deconstructeur
-template <class TYPE>
+template<class TYPE>
 pile<TYPE>::~pile()
 {
-	clear();
+	_top = nullptr;
+	_size = 0;
 }
 
-//Ajoute un nouveau dessus
-template <class TYPE>
-void pile<TYPE>::push(const TYPE& e)
-{
-	next = new cellule(e, premier);
-}
-
-//Enlève le dessus
-template <class TYPE>
-TYPE pile<TYPE>::pop()
-{
-	assert(!empty());
-	TYPE valeur = _top->element;
-	cellule *aDetruire = _top;
-	_top = _top->next;
-	delete aDetruire;
-	return valeur;
-}
-
-//Si la pile est vide
-template <class TYPE>
-bool pile<TYPE>::empty() const
-{
-	return _top == nullptr;
-}
-
-//Vide la pile
-template <class TYPE>
-void pile<TYPE>::clear()
-{
-	while (!empty())pop();
-}
-
-//Retourne le dessus, mais dépile pas
-template <class TYPE>
-const TYPE& pile<TYPE>::top() const
-{
-	assert(!empty());
-	return _top->element;
-}
-
-//Retourne le dessus, mais en setteur
 template<class TYPE>
-TYPE& pile<TYPE>::top()
+void pile<TYPE>::push(const TYPE & e)
 {
-	assert(!empty());
+	_size++;
+	_top = new cellule(e, _top);
+}
+
+//Enlève le premier élément dans la pile
+template<class TYPE>
+void pile<TYPE>::pop()
+{
+	assert(_size > 0);
+	_size--;
+
+	cellule *temp = _top;
+	_top = _top->next;
+
+	delete temp;
+}
+
+template<class TYPE>
+const TYPE & pile<TYPE>::top() const
+{
+	assert(_size > 0);
 	return _top->element;
 }
 
-//Retourne le nb d’élément
-template <class TYPE>
+template<class TYPE>
+TYPE & pile<TYPE>::top()
+{
+	assert(_size > 0);
+	return _top->element;
+}
+
+template<class TYPE>
 int pile<TYPE>::size() const
 {
 	return _size;
 }
 
-//Affectateur
-template <class TYPE>
-const pile<TYPE>& pile<TYPE>::operator=(const pile& s)
+template<class TYPE>
+bool pile<TYPE>::isEmpty() const
 {
-	if (this == &src)
-	{
-		return *this;		//autoallocation
-	}
-
-	clear();
-
-	if (!s.empty())
-	{
-		cellule* 	tempI, tempE = p._top;
-
-		tempI = _top = new cellule(tempE->element, nullptr);
-		tempE = tempE->next;
-
-		while (tempE != nullptr)
-		{
-			tempI = tempE->next = new cellule(tempE->element, nullptr);
-			tempE = tempE->next;
-		}
-	}
-	return *this;		//retour
+	return _top == nullptr;
 }
 
-//Comparaison de 2 piles
 template<class TYPE>
-bool pile<TYPE>::operator==(const pile & s) const
+void pile<TYPE>::clear()
 {
-	return false;
+	while (!empty())
+		pop();
+}
+
+//template<class TYPE>
+//const pile<TYPE>& pile<TYPE>::operator=(const pile& p)
+//{
+//	if (this == &src)
+//		return *this;
+//
+//	clear();
+//	if (!p.isEmpty())
+//	{
+//		cellule* tempI;
+//		cellule* tempE = p._top;
+//
+//		tempI = _top = new cellule(tempE->element, nullptr);
+//		tempE = tempE->next;
+//
+//		while (tempE != nullptr)
+//		{
+//			tempI = tempE->next = new cellule(tempE->element, nullptr);
+//			tempE = tempE->next;
+//		}
+//
+//		_size = p.size();
+//	}
+//	return  *this;
+//}
+
+//template<class TYPE>
+//bool pile<TYPE>::operator==(const pile & p) const
+//{
+//	if (_size == p.size())
+//	{
+//		cellule* temp1 = p.top();
+//		cellule* temp2 = top();
+//
+//		for (int i = 0; i < _size; i++)
+//		{
+//			if (temp1->element != temp2->element)
+//			{
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+//	return false;
+//}
+
+template<class TYPE>
+pile<TYPE>::pile(const pile<TYPE>& p)
+{
 }
